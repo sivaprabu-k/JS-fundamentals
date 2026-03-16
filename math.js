09a3f76dde06a349686001ffc82c3e65208a2d87
sivaprabu-k <sivaprabu.k@mitrahsoft.in>
2026-03-16 14:00:43 +0530

    added divide function in math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+function add(x,y){
+    return x+y;
+}
+console.log(add(10,5));
+
+function subtract(x,y){
+    return x-y;
+}
+console.log(subtract(10,5));
+
+function multiply(x,y){
+    return x*y;
+}
+console.log(multiply(10,5));
