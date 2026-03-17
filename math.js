08b55e3ed64617495bc99dacadbb3a49c6bad969
sivaprabu-k <sivaprabu.k@mitrahsoft.in>
2026-03-17 11:56:02 +0530

    modified multiply function and added divide function

--- math.js
+++ math.js
@@ -8,7 +8,12 @@ function subtract(x,y){
 }
 console.log(subtract(10,5));
 
-function multiply(x,y){
-    return x*y;
+function multiply(a,b){
+    return a*b;
 }
-console.log(multiply(10,5));
+console.log(multiply(20,10));
+
+function divide(x,y){
+    return x/y;
+}
+console.log(divide(10,5));
